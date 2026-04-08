7de3aaec322a1b79ba0d0d825996b6a035e7a4d9
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2026-04-08 21:54:17 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import OmevoLogin from './components/OmevoLogin';
-import TermsOfService from './components/TermsOfService';
-import OmevoChat from './components/OmevoChat';
-import OmevoVideoApp from './components/OmevoVideoApp';
+import OmevoLogin from './pages/Home';
+import TermsOfService from './pages/Terms';
+import OmevoChat from './pages/OmevoChat';
+import OmevoVideoApp from './pages/OmevoVideoApp';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* Public Routes */}
-        <Route path="/" element={<OmevoLogin />} />
-        <Route path="/terms" element={<TermsOfService />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/terms" element={<Terms />} />
         
         {/* App Routes */}
         <Route path="/chat" element={<OmevoChat />} />
