427b25b21d2e599bb222bddb73ac2a2777fe558e
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2026-04-08 23:43:45 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: '.',          // project root
+  plugins: [react()],
+  base: './',      // makes paths relative for Vercel
   build: {
-    outDir: 'dist',   // inside project root
-    emptyOutDir: true // cleans dist before build
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
