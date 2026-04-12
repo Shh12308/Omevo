ac51b2cf5235df931dc225317a4f3c7bd82f7a83
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2026-04-12 17:29:56 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
+import Video from './pages/Video';
+import Chat from './pages/Chat';
+import Terms from './pages/Terms';
 
 
 function App() {
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         {/* Public Routes */}
         <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/video" element={<Video />} />
+        <Route path="/terms" element={<Terms />} />
         
         {/* Fallback */}
         <Route path="*" element={<Navigate to="/" replace />} />
